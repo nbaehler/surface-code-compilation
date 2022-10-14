43bf5591f5465ffeb4e13605ee8da6b763af2159
nbaehler <nicolas.bahler@epfl.ch>
2022-10-14 16:49:07 +0200

    Test if llvm works with the qir

--- code/compiler.py
+++ code/compiler.py
@@ -12,21 +12,41 @@ from Circuits import testCircuit
 
 
 def main():
+    in_qir = testCircuit.as_qir()
+
+    with tempfile.NamedTemporaryFile(
+        suffix=".ll"
+    ) as f:  # https://github.com/qir-alliance/pyqir/blob/a99afdb8126b1ff0a331bdc81aed9930f7bd23b9/examples/evaluator/teleport.py#L36-L40
+        f.write(in_qir.encode("utf-8"))
+        f.flush()
+        cnots = logGates(f.name)
+
+    # -------------------------------
+
     # in_qir = testCircuit.as_qir()
 
-    # with tempfile.NamedTemporaryFile(
-    #     suffix=".ll"
-    # ) as f:  # https://github.com/qir-alliance/pyqir/blob/a99afdb8126b1ff0a331bdc81aed9930f7bd23b9/examples/evaluator/teleport.py#L36-L40
-    #     f.write(in_qir.encode("utf-8"))
-    #     f.flush()
-    #     cnots = logGates(f.name)
+    # with open("temp.ll", "w") as f:
+    #     f.writelines(in_qir)
 
-    path = Path(__file__).parent
-    file_path = os.path.join(path, "bernstein_vazirani.bc")
+    # os.system("llvm-as temp.ll")
 
-    cnots = logGates(file_path)
+    # cnots = logGates("temp.bc")
+
+    # os.remove("temp.ll")
+    # os.remove("temp.bc")
+
+    # -------------------------------
+
+    # path = Path(__file__).parent
+    # file_path = os.path.join(path, "bernstein_vazirani.bc")
+
+    # cnots = logGates(file_path)
+
+    # -------------------------------
 
     # cnots = [(0, 1), (15, 1)]
+
+    # -------------------------------
 
     grid_dims = (4, 5)
 
